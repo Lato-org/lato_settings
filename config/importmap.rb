f67a1537ee218ea8d1b42a260196e1f73a3d8ec6
Gregorio Galante <me@gregoriogalante.com>
2025-03-29 16:19:41 +0100

    Added custom js controller

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,1 +1,2 @@
 pin "lato_settings/application", to: "lato_settings/application.js"
+pin_all_from LatoSettings::Engine.root.join("app/assets/javascripts/lato_settings/controllers"), under: "controllers", to: "lato_settings/controllers"
